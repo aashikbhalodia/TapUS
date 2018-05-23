a3b5898cf3b35798a3ab409a888234597c4c49af
Aashik <abhalodia2018@mka.org>
2018-05-23 11:34:03 -0400

    Added a small print statement

--- TapUS/ViewController.swift
+++ TapUS/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Hey what's up hello")
     }
 
     override func didReceiveMemoryWarning() {
